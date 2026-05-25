9d2b5aff7d59d8bfe88e9bd5cf4cf304e3619a2d
jefferson custodio <43219830+jefferson-custodio@users.noreply.github.com>
2026-05-25 14:43:39 -0300

    fix: correct import path for FundAccess in user.entity.ts

--- src/app/users/entities/user.entity.ts
+++ src/app/users/entities/user.entity.ts
@@ -6,7 +6,7 @@ import {
   comparePassword,
 } from '../../../common/utils/password.utils';
 import { AuthLoginAttempt } from '../../auth/entities/auth_login_attempt.entity';
-import { FundAccess } from 'src/app/funds/entities/user-fund-access.entity';
+import { FundAccess } from '../../funds/entities/user-fund-access.entity';
 @Entity()
 export class User extends BaseAppEntity {
   @OneToMany(() => FundAccess, (fundAccess) => fundAccess.user)
